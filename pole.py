29ed84c3596976a14ef79b8708f97fdec36e4623
Whiteside <vovadxc@gmail.com>
2024-03-19 13:49:30 +0100

    Pole

---
+++ pole.py
@@ -0,0 +1,7 @@
+arrays = ["Ford", "Mustang", "BMW", "Porshe", "Audi"]
+
+for i in arrays:
+    print (i)
+
+for x in arrays:
+    print("1: ", arrays[0])
---
+++ tests.py
@@ -0,0 +1,4 @@
+arrays = ["Ford", "Mustang", "BMW", "Porshe", "Audi"]
+
+for x in range(len(arrays)):
+    print(f"{x+1}: {arrays[x]}")
