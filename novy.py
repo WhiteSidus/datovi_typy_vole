0ce9f2659188a4312b391b66efe223cd81767465
Whiteside <vovadxc@gmail.com>
2024-04-19 08:49:44 +0200

    ahha

---
+++ novy.py
@@ -0,0 +1,7 @@
+pole1 = [3,2,1,5,4]
+
+def seradit(x):
+    x = sorted(pole1)
+    print(x)
+
+seradit(pole1)
---
+++ seradeni.py
@@ -0,0 +1,5 @@
+pole1 = [3,2,1,5,4]
+
+def seredit(pole1):
+    for i in pole1:
+        
